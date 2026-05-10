4d5576ff2353c407e757b29ac1b680e850a324f4
karinafatahbinway18 <karinafatah06@gmail.com>
2026-05-10 17:34:37 +0700

    perbaikan config dan proses proses_register

--- auth/proses_register.php
+++ auth/proses_register.php
@@ -1,18 +1,16 @@
 <?php
 
-include '../config/koneksi.php';
+include '.. /config/koneksi.php';
 
 $nama = $_POST['nama'];
 $username = $_POST['username'];
 $password = md5($_POST['password']);
 
 $query = "INSERT INTO users VALUES 
-(NULL, '$nama', '$username', '$password')";
-
+(NULL,'$nama','$username','$password')";
 
 mysqli_query($conn, $query);
 
-
-header("location: login.php");
+header("Location: login.php");
 
 ?>
--- config/koneksi.php
+++ config/koneksi.php
@@ -1,7 +1,9 @@
 <?php
-$conn =mysqli_connect("localhost", "root", "" ,"perpustakaan");
-if (!$conn){
-    die("Koneksi gagal:" . mysqli_connect_error());
+
+$conn = mysqli_connect("localhost","root","","perpustakaan");
+
+if(!$conn){
+    die("Koneksi gagal");
 }
 
 ?>
