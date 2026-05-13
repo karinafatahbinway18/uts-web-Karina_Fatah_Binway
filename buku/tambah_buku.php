505fae7c3c2b371b2e3bd55ca314fef5fc0c6b1c
karinafatahbinway18 <karinafatah06@gmail.com>
2026-05-13 10:28:52 +0700

    perbaikan tambah buku

--- buku/tambah_buku.php
+++ buku/tambah_buku.php
@@ -12,10 +12,11 @@
                 <h2>Tambah buku</h2>
 
                 <form action="proses_tambah.php" method="POST">
-                    <input type="text" name="judul" placeholder="Judul Buku"> <br><br>
+                    <input type="text" name="judul" placeholder="Judul Buku"required> <br><br>
                     <input type="text" name="penulis" placeholder="Penulis" required><br><br>
                     <input type="text" name="penerbit" placeholder="Penerbit" required><br><br>
-                    <input type="text" name="tahun" placeholder="Tahun" required><br><br>
+                    <input type="text" name="tahun_terbit" placeholder="Tahun Terbit" required><br><br>
+                    <input type="text" name="kategori" placeholder="Kategori"required><br><br>
 
                     <button type="submit">Simpan</button>
                 </form>
