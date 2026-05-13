46f6e26c5736280e1518570cf6301b1216de2319
karinafatahbinway18 <karinafatah06@gmail.com>
2026-05-13 15:48:33 +0700

    perbaikan tambaah buku

--- buku/tambah_buku.php
+++ buku/tambah_buku.php
@@ -2,7 +2,7 @@
 <html>
     <head>
         <title>Tambah buku</title>
-        <link rel="stylesheet" href="../assets/style.css">
+        <link rel="stylesheet" href="../assets/CSS/style.css">
     </head>
     <body>
         <div class="container">
