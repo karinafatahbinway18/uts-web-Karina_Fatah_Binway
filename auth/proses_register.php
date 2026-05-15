0ab37a70c539f1584a5b8ce4dc2021c242acfb4e
karinafatahbinway18 <karinafatah06@gmail.com>
2026-05-15 15:17:32 +0700

    pada proses_registertambah role admin dan user

--- auth/proses_register.php
+++ auth/proses_register.php
@@ -1,24 +1,19 @@
 <?php
-
 include '../config/koneksi.php';
 /** @var mysqli $conn */
 
 $nama = $_POST['nama'];
 $username = $_POST['username'];
 $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+$role = "user";
 
 $query = mysqli_query($conn,
 "INSERT INTO users VALUES
-(NULL, '$nama', '$username', '$password')");
+(NULL,'$nama','$username','$password','$role')");
 
 if($query){
-
     header("Location: login.php");
-
 }else{
-
     echo "Register gagal";
-
 }
-
 ?>
